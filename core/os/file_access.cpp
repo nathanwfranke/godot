5b41ebec3c9abc2ff5b5a0573a0f9f020eff3f8e
Nathan Franke <natfra@pm.me>
2020-08-27 16:25:57 -0500

    Fix CSV Invalid PackedStringArray

--- core/os/file_access.cpp
+++ core/os/file_access.cpp
@@ -362,7 +362,9 @@ Vector<String> FileAccess::get_csv_line(const String &p_delim) const {
 		}
 	}
 
-	strings.push_back(current);
+	if (l.length()) {
+		strings.push_back(current);
+	}
 
 	return strings;
 }
